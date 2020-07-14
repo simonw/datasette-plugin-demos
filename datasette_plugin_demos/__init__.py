22af14ed2c273934c54707306f0e113ca5a68668
Simon Willison <swillison@gmail.com>
2020-07-13 20:59:10 -0700

    Fix package_data, apply black - closes #1

--- datasette_plugin_demos/__init__.py
+++ datasette_plugin_demos/__init__.py
@@ -4,9 +4,9 @@ import random
 
 @hookimpl
 def prepare_jinja2_environment(env):
-    env.filters['uppercase'] = lambda u: u.upper()
+    env.filters["uppercase"] = lambda u: u.upper()
 
 
 @hookimpl
 def prepare_connection(conn):
-    conn.create_function('random_integer', 2, random.randint)
+    conn.create_function("random_integer", 2, random.randint)
--- setup.py
+++ setup.py
@@ -1,27 +1,18 @@
 from setuptools import setup
 
-VERSION = '0.3'
+VERSION = "0.3"
 
 setup(
-    name='datasette-plugin-demos',
-    description='Examples of plugins for Datasette',
-    author='Simon Willison',
-    url='https://github.com/simonw/datasette-plugin-demos',
-    license='Apache License, Version 2.0',
+    name="datasette-plugin-demos",
+    description="Examples of plugins for Datasette",
+    author="Simon Willison",
+    url="https://github.com/simonw/datasette-plugin-demos",
+    license="Apache License, Version 2.0",
     version=VERSION,
-    packages=['datasette_plugin_demos'],
-    entry_points={
-        'datasette': [
-            'plugin_demos = datasette_plugin_demos'
-        ]
-    },
+    packages=["datasette_plugin_demos"],
+    entry_points={"datasette": ["plugin_demos = datasette_plugin_demos"]},
     package_data={
-        'datasette_plugin_demos': [
-            'static/plugin.js',
-        ],
-        'datasette_plugin_demos': [
-            'templates/show_json.html',
-        ],
+        "datasette_plugin_demos": ["static/plugin.js", "templates/show_json.html"],
     },
-    install_requires=['datasette']
+    install_requires=["datasette"],
 )
